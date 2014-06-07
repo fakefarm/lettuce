26086f8d5394cff15f2d306438e5421d2ad2a81b
Dave Woodall <dmwoodall@gmail.com>
2014-06-07 08:04:31 -0600

    add devise to people engine

--- apps/people/lib/people/engine.rb
+++ apps/people/lib/people/engine.rb
@@ -1,6 +1,9 @@
+require 'devise'
+
 module People
   class Engine < ::Rails::Engine
     isolate_namespace People
+
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
