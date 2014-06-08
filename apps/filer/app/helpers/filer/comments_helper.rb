b1b95821812f855b8d1c20514ed5070ed9a205ab
Dave Woodall <dmwoodall@gmail.com>
2014-06-08 10:12:07 -0600

    comment styling

--- apps/filer/app/helpers/filer/comments_helper.rb
+++ apps/filer/app/helpers/filer/comments_helper.rb
@@ -1,4 +1,12 @@
 module Filer
   module CommentsHelper
+    def month_name(comment)
+      source = Filer::Month.find(comment.source.month_id)
+      source.name
+    end
+
+    def mo_path(comment)
+      month_path(comment.source.month_id)
+    end
   end
 end
