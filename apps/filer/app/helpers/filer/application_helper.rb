47f9a486dc2f39f295b4ea00753bbf8b3c2448d5
Dave Woodall <dmwoodall@gmail.com>
2014-06-09 05:51:37 -0600

    Page styling to Month and Source, including file_types helper

--- apps/filer/app/helpers/filer/application_helper.rb
+++ apps/filer/app/helpers/filer/application_helper.rb
@@ -1,4 +1,7 @@
 module Filer
   module ApplicationHelper
+    def file_types
+      [['(please select)', ''], ['Source file', 'source file'], ['Monthly Report', 'Monthly report'], ['Invoice', 'invoice'], ['Tax related', 'tax related'], ['QuickBooks backup', 'QuickBooks backup'], ['Receipt', 'Receipt'], ]
+    end
   end
 end
