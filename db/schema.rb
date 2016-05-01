123780baf03648dfb4fc50f006a128c8a211850a
woodall <dave@woodalls.me>
2016-05-01 14:28:14 -0400

    Install clearance (part 1)
    Following along with Clearnance readme before installing views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430051448) do
+ActiveRecord::Schema.define(version: 20160501181353) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -148,6 +148,18 @@ ActiveRecord::Schema.define(version: 20160430051448) do
 
   add_index "transfers", ["family_id"], name: "index_transfers_on_family_id"
   add_index "transfers", ["user_id"], name: "index_transfers_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
   create_table "vendors", force: :cascade do |t|
     t.string   "name"
