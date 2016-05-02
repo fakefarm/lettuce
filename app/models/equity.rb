3743b71f8abf25bc89907af82802097127b52c78
woodall <dave@woodalls.me>
2016-05-02 00:11:35 -0400

    Assign_current_balance after_create

--- app/models/equity.rb
+++ app/models/equity.rb
@@ -1,4 +1,11 @@
 class Equity < ActiveRecord::Base
   has_many :transfers
   has_many :spends
+
+  before_create :assign_current_balance
+
+private
+  def assign_current_balance
+    self.current_balance = self.starting_balance
+  end
 end
--- app/models/liability.rb
+++ app/models/liability.rb
@@ -1,2 +1,8 @@
 class Liability < ActiveRecord::Base
+  before_create :assign_current_balance
+
+private
+  def assign_current_balance
+    self.current_balance = self.starting_balance
+  end
 end
