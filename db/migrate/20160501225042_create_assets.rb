cb9a6ec8c5bc96b792b70b6d31acfd1281f1becd
woodall <dave@woodalls.me>
2016-05-03 23:58:42 -0400

    Add user_id to Asset

--- db/migrate/20160501225042_create_assets.rb
+++ db/migrate/20160501225042_create_assets.rb
@@ -4,8 +4,10 @@ class CreateAssets < ActiveRecord::Migration
       t.integer :equity_id
       t.integer :liability_id
       t.integer :balance
+      t.integer :user_id
 
       t.timestamps null: false
     end
+    add_index :assets, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,12 @@ ActiveRecord::Schema.define(version: 20160503004445) do
     t.integer  "equity_id"
     t.integer  "liability_id"
     t.integer  "balance"
+    t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  add_index "assets", ["user_id"], name: "index_assets_on_user_id"
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
