c9d444d2cdf66506b25f5ab85660a36327054c70
woodall <dave@woodalls.me>
2016-05-04 01:06:06 -0400

    Add after_create and after_update hooks...
    to equities and liabilities so they stay in sync with assets.

--- app/models/equity.rb
+++ app/models/equity.rb
@@ -5,9 +5,19 @@ class Equity < ActiveRecord::Base
   has_many :streams
 
   before_create :assign_current_balance
+  after_create  :create_asset
+  after_update :update_asset_balance
 
 private
   def assign_current_balance
     self.current_balance = self.starting_balance
+  end
+
+  def create_asset
+    Asset.create(equity_id: self.id, balance: self.starting_balance, user_id: self.user_id)
+  end
+
+  def update_asset_balance
+    Asset.find_by_equity_id(self.id).update_attribute('balance', self.current_balance)
   end
 end
--- app/models/liability.rb
+++ app/models/liability.rb
@@ -1,8 +1,18 @@
 class Liability < ActiveRecord::Base
   before_create :assign_current_balance
+  after_create  :create_asset
+  after_update :update_asset_balance
 
 private
   def assign_current_balance
     self.current_balance = self.starting_balance
+  end
+
+  def create_asset
+    Asset.create(liability_id: self.id, balance: self.starting_balance, user_id: self.user_id)
+  end
+
+  def update_asset_balance
+    Asset.find_by_equity_id(self.id).update_attribute('balance', self.current_balance)
   end
 end
